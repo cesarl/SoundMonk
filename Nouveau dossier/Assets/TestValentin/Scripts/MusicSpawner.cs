c775863aab875f05ec3e87f9976a1af2e7505af9
bas_v <v.bas@student.isartdigital.com>
2013-11-18 15:40:22 +0100

    start spawner

---
+++ Nouveau dossier/Assets/TestValentin/Scripts/MusicSpawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+
+public class MusicSpawner : MonoBehaviour
+{
+	public float[] times;
+	public string[] name;
+
+	private GameObject[]	spawners;
+	private float			lastTime;
+
+	// Use this for initialization
+	void Start () {
+		spawners = GameObject.FindGameObjectsWithTag("Spawner");
+		lastTime = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
