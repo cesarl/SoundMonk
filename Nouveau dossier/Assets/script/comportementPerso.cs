027efe35e369734eff26d9dfdd8e444313935d44
ben-128 <ben.maurin@gmail.com>
2013-11-18 22:18:30 +0100

    on s'en fou

---
+++ Nouveau dossier/Assets/script/comportementPerso.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class comportementPerso : MonoBehaviour
+{
+
+    public int vie;
+    public int viePerduParObstacle;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.tag == "obstacle")
+        {
+            vie -= viePerduParObstacle;
+
+        }
+    }
+
+
+
+}
