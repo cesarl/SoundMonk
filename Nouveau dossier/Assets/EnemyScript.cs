5f30a353772d567af7bac3cb34286523aaee2ba2
Cesar Leblic <cesar.leblic@gmail.com>
2013-11-18 15:08:18 +0100

    Enemy follow player and collision detection

---
+++ Nouveau dossier/Assets/EnemyScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript
+    : MonoBehaviour {
+
+    public float speed = 1.0f;
+    GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+        _player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, _player.transform.position, speed * Time.deltaTime);
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.name == "Player")
+            Destroy(this.gameObject);
+    }
+}
