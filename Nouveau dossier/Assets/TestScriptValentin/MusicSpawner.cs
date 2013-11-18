05569af0f785362d67db1aed775332c9834e10ed
bas_v <v.bas@student.isartdigital.com>
2013-11-18 15:10:28 +0100

    test character controller

---
+++ Nouveau dossier/Assets/TestScriptValentin/MusicSpawner.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicSpawner : MonoBehaviour {
+
+	public Transform[] spawners;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
