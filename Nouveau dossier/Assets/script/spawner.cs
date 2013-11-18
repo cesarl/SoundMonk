be85547ab2541515f83210fd808c56a64b9c04c6
ben-128 <ben.maurin@gmail.com>
2013-11-18 21:03:24 +0100

    changements spawn

--- Nouveau dossier/Assets/script/spawner.cs
+++ Nouveau dossier/Assets/script/spawner.cs
@@ -7,38 +7,62 @@ public class spawner : MonoBehaviour {
     public int nbNotes;
       
     public float tempsEntreSpawn;
+    public int[] codeNotes;
+
 
     private float delay;
-    private bool collided =false;
+    private bool collided = false;
+	private SpriteRenderer spriteRenderer;
+	private GameObject camera;
+   private int cpt = 0;
+
+
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+		camera = GameObject.FindGameObjectWithTag("MainCamera") as GameObject;
+       // codeNotes = new int[nbNotes];
+
 	}
 
-    void OnTriggerEnter(Collider other)
-    {
-       // Debug.Log("test");
-        collided = true;
-        delay = 0.0f;
-    }
 
     // Update is called once per frame
     void Update()
     {
+        GameObject go;
+
+        if (IsVisibleFrom(spriteRenderer, camera.camera))
+        {
+            collided = true;
+            delay = 0.0f;
+        }
+       
         if (collided)
         {
             delay += Time.deltaTime;
 
             if (delay >= tempsEntreSpawn)
             {
+
                 nbNotes--;
-                Instantiate(note, transform.position, Quaternion.identity);
+               go = Instantiate(note, transform.position, Quaternion.identity) as GameObject;
+               go.GetComponent<noteSon>().typeSon = codeNotes[cpt];
+
                 delay = 0.0f;
+                cpt++;
             }
             if (nbNotes <= 0)
             {
                 DestroyImmediate(this);
             }
         }
+	}
 
+	private bool IsVisibleFrom(this SpriteRenderer renderer, Camera camera)
+	{
+		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
 	}
 }
