e278d22405405038ddc364a18aed61ad13ff1764
ben-128 <ben.maurin@gmail.com>
2013-11-18 15:12:00 +0100

    script distance son note

---
+++ Nouveau dossier/Assets/script/lireFichier.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lireFichier : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//	Resources.Load()
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Nouveau dossier/Assets/script/noteBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+
+    public Transform target;
+    private Vector3 spawner;
+
+	// Use this for initialization
+	void Start () {
+        spawner = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Gets a vector that points from the player's position to the target's.
+
+        float distanceSpawnerMoine = Vector3.Distance(transform.position, spawner);
+        float distanceObjetMoine = Vector3.Distance(transform.position, target.position);
+
+
+        transform.audio.volume = (distanceSpawnerMoine - distanceObjetMoine) / distanceSpawnerMoine;
+
+	}
+}
