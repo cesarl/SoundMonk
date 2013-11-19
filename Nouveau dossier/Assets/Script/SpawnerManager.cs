afff1c947e4933c83f56810656461f5177613a32
bas_v <v.bas@student.isartdigital.com>
2013-11-19 12:06:17 +0100

    spawn manager

--- Nouveau dossier/Assets/Script/SpawnerManager.cs
+++ Nouveau dossier/Assets/Script/SpawnerManager.cs
@@ -1,15 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+using UnityEditor;
 public class SpawnerManager : MonoBehaviour {
+
+	public GameObject[] spawners;
+	public GameObject[] targets;
+	public UnityEngine.Object filePattern;
+	public Sprite[] sprites;
+
+	private int idx = 0;
+	private int nbNotes;
+	private float delay;
+
+	BellScript mbs_bellScript;
+
+	public GameObject note;
+
+	public AudioClip[] sounds;
+
+	[Serializable]
+	public class PatternNote
+	{
+		public float timeSpawn;
+		public int type;
+		public int damage;
+		public int idTarget;
+		public int idSound;
+		public int idSoundDestruction;
+		public int idSprite;
+		public float speed;
+	}
+
+	List<PatternNote> notes;
 
 	// Use this for initialization
 	void Start () {
-	
+		loadPattern(AssetDatabase.GetAssetPath(filePattern));
+		mbs_bellScript = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<BellScript>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		GameObject go;
+
+		if (notes == null)
+			return;
+		if (notes.Count != 0 && idx < notes.Count)
+		{
+			delay += Time.deltaTime;
+			if (delay >= notes[idx].timeSpawn)
+			{
+				Vector3 pos = spawners[notes[idx].idTarget].transform.position;
+				go = Instantiate(note, pos, Quaternion.identity) as GameObject;
+				mbs_bellScript.SendMessage("CalculateAccuracy");
+				EnemyScript son = go.GetComponent<EnemyScript>();
+				son.typeSon = notes[idx].type;
+				son.damage = notes[idx].damage;
+				son.target = targets[notes[idx].idTarget].transform;
+				son.audio.clip = sounds[notes[idx].idSound];
+				son.audio.Play();
+				son.speed = notes[idx].speed;
+				//son.Destruction = sounds[notes[idx].idSound];
+				son.GetComponent<SpriteRenderer>().sprite = sprites[notes[idx].idSprite];
+				idx++;
+			}
+		}
 	}
+
+	private bool loadPattern(string file)
+	{
+		XmlSerializer mySerializer = new XmlSerializer(typeof(List<PatternNote>));
+		using (FileStream reader = new FileStream(file, FileMode.Open))
+		{
+			notes = mySerializer.Deserialize(reader) as List<PatternNote>;
+		}
+		return true;
+	}
+
 }
--- Nouveau dossier/Assets/script/spawner.cs
+++ Nouveau dossier/Assets/script/spawner.cs
@@ -9,100 +9,15 @@ using UnityEditor;
 public class spawner : MonoBehaviour
 {
 
-	public GameObject note;
-
-	public UnityEngine.Object filePattern;
-	public AudioClip[] sounds;
-	public GameObject target;
-
-	private float delay;
-	private bool collided = false;
-	private SpriteRenderer spriteRenderer;
-	private Camera myCamera;
-	private int idx = 0;
-	private int nbNotes;
-
-    BellScript mbs_bellScript;
-
-	[Serializable]
-	public class PatternNote
-	{
-		public float timeSpawn;
-		public int type;
-		public int damage;
-		public int idTarget;
-		public int idSound;
-		public float speed;
-	}
-
-	List<PatternNote> notes;
-
 	// Use this for initialization
 	void Start()
 	{
-		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
-		myCamera = Camera.main;
-		loadPattern(AssetDatabase.GetAssetPath(filePattern));
-        mbs_bellScript = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<BellScript>();
 	}
 
 
 	// Update is called once per frame
 	void Update()
 	{
-		GameObject go;
-
-		if (notes == null)
-			return;
-		if (IsVisibleFrom(spriteRenderer, myCamera.camera) && !collided)
-		{
-			collided = true;
-			delay = 0.0f;
-		}
-
-		if (collided && notes.Count != 0)
-		{
-			delay += Time.deltaTime;
-			if (delay >= notes[idx].timeSpawn)
-			{
-				go = Instantiate(note, transform.position, Quaternion.identity) as GameObject;
-                mbs_bellScript.SendMessage("CalculateAccuracy");
-				EnemyScript son = go.GetComponent<EnemyScript>();
-				son.typeSon = notes[idx].type;
-				son.damage = notes[idx].damage;
-				son.target = target.transform;
-				son.audio.clip = sounds[notes[idx].idSound];
-				son.audio.Play();
-				son.speed = notes[idx].speed;
-				idx++;
-			}
-			if (idx >= notes.Count)
-				DestroyImmediate(this);
-		}
-	}
-
-	private bool IsVisibleFrom(this SpriteRenderer renderer, Camera camera)
-	{
-		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
-		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
-	}
-
-	private bool savePattern(string file)
-	{
-		XmlSerializer mySerializer = new XmlSerializer(typeof(List<PatternNote>));
-		using (StreamWriter writer = new StreamWriter(file))
-		{
-			mySerializer.Serialize(writer, notes);
-		}
-		return true;
-	}
-	private bool loadPattern(string file)
-	{
-		XmlSerializer mySerializer = new XmlSerializer(typeof(List<PatternNote>));
-		using (FileStream reader = new FileStream(file, FileMode.Open))
-		{
-			notes = mySerializer.Deserialize(reader) as List<PatternNote>;
-		}
-		return true;
+		
 	}
 }
