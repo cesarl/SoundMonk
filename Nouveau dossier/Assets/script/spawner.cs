999e66227e3290bb461df47de7cd4c9925ae0853
bas_v <v.bas@student.isartdigital.com>
2013-11-19 03:44:04 +0100

    xml

--- Nouveau dossier/Assets/script/spawner.cs
+++ Nouveau dossier/Assets/script/spawner.cs
@@ -1,29 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+using System;
+using UnityEditor;
 
 public class Spawner : MonoBehaviour
 {
 
 	public GameObject note;
-	public int nbNotes;
 
-	public float tempsEntreSpawn;
-	public int[] codeNotes;
-
+	public UnityEngine.Object filePattern;
 
 	private float delay;
 	private bool collided = false;
 	private SpriteRenderer spriteRenderer;
 	private GameObject camera;
-	private int cpt = 0;
+	private int idx = 0;
+	private int nbNotes;
+
+	[Serializable]
+	public class PatternNote
+	{
+		public float timeSpawn;
+		public int type;
+		public int damage;
+	}
+
+	public List<PatternNote> notes;
 
 	// Use this for initialization
 	void Start()
 	{
 		spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
 		camera = GameObject.FindGameObjectWithTag("MainCamera") as GameObject;
-		// codeNotes = new int[nbNotes];
-
+		loadPattern(AssetDatabase.GetAssetPath(filePattern));
 	}
 
 
@@ -32,33 +44,25 @@ public class Spawner : MonoBehaviour
 	{
 		GameObject go;
 
+		if (notes == null)
+			return;
 		if (IsVisibleFrom(spriteRenderer, camera.camera) && !collided)
 		{
 			collided = true;
 			delay = 0.0f;
-		//	Debug.Log("visible");
 		}
 
 		if (collided)
 		{
 			delay += Time.deltaTime;
-			//Debug.Log("delay:" + delay);
-			if (delay >= tempsEntreSpawn)
+			if (delay >= notes[idx].timeSpawn)
 			{
-			//	Debug.Log("in if");
-				nbNotes--;
 				go = Instantiate(note, transform.position, Quaternion.identity) as GameObject;
-				go.GetComponent<noteSon>().typeSon = codeNotes[cpt];
-				delay = 0.0f;
-
-                //GameObject.Find("Cloche").GetComponent<comportementPerso>().SendMessage("coucou j appele ta methode mon copain !");
-				cpt++;
+				go.GetComponent<noteSon>().typeSon = notes[idx].type;
+				idx++;
 			}
-			if (nbNotes <= 0)
-			{
-			//	Debug.Log("delete");
+			if (idx >= notes.Count)
 				DestroyImmediate(this);
-			}
 		}
 	}
 
@@ -66,5 +70,25 @@ public class Spawner : MonoBehaviour
 	{
 		Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
 		return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+	}
+
+	private bool savePattern(string file)
+	{
+		XmlSerializer mySerializer = new XmlSerializer(typeof(List<PatternNote>));
+		using (StreamWriter writer = new StreamWriter(file))
+		{
+			mySerializer.Serialize(writer, notes);
+		}
+		return true;
+	}
+	private bool loadPattern(string file)
+	{
+		XmlSerializer mySerializer = new XmlSerializer(typeof(List<PatternNote>));
+		using (FileStream reader = new FileStream(file, FileMode.Open))
+		{
+			notes = mySerializer.Deserialize(reader) as List<PatternNote>;
+		}
+		Debug.Log(notes.Count);
+		return true;
 	}
 }
